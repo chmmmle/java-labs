3507e10b2d3f7c13e06491d1c10467a31aa00a65
kamilya <120671774+chmmmle@users.noreply.github.com>
2025-02-02 15:51:01 +0200

    Create task2.java

---
+++ lab1/task2.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class AlternatingSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Вводим количество чисел
+        int n = scanner.nextInt();
+
+        // Инициализируем переменную для суммы
+        int sum = 0;
+
+        // Читаем числа и вычисляем знакочередующуюся сумму
+        for (int i = 0; i < n; i++) {
+            int num = scanner.nextInt();
+            
+            // Если индекс четный (начинаем с 0), прибавляем число, если нечетный - вычитаем
+            if (i % 2 == 0) {
+                sum += num;
+            } else {
+                sum -= num;
+            }
+        }
+
+        // Выводим результат
+        System.out.println(sum);
+
+        // Закрываем scanner
+        scanner.close();
+    }
+}
