f759b13b82c3d31943e90e9c3331d2342b47732d
kamilya <120671774+chmmmle@users.noreply.github.com>
2025-02-02 15:53:56 +0200

    Create task4.java

---
+++ lab1/task4.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+
+public class Roads {
+public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int n = scanner.nextInt();
+        
+        int maxHeight = -1; // минимальная высота
+        int bestRoad = -1;  // Номер дороги минимальной высотой
+        
+        // Перебор всех дорог
+        for (int roadNumber = 1; roadNumber <= n; roadNumber++) {
+            // Чтение количества туннелей на текущей дороге
+            int m = scanner.nextInt();
+            
+            int minHeight = Integer.MAX_VALUE; 
+            
+            // Чтение высот туннелей
+            for (int i = 0; i < m; i++) {
+                int tunnelHeight = scanner.nextInt();
+                if (tunnelHeight < minHeight) {
+                    minHeight = tunnelHeight; // Находим минимальную высоту на этой дороге
+                }
+            }
+            
+            // Если минимальная высота на текущей дороге больше, чем на предыдущей, обновляем результат
+            if (minHeight > maxHeight) {
+                maxHeight = minHeight;
+                bestRoad = roadNumber;
+            }
+        }
+        
+        // Выводим номер дороги и максимальную минимальную высоту
+        System.out.println(bestRoad + " " + maxHeight);
+    }
+}
