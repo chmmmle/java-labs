d7418453d7e23a2a9627a3152e4cb42a29fcd837
kamilya <120671774+chmmmle@users.noreply.github.com>
2025-02-06 20:12:19 +0200

    Update task5.java

--- lab2/task5.java
+++ lab2/task5.java
@@ -1,1 +1,34 @@
+import java.util.HashSet;
+
+public class PairSum {
+    public static int[] findPair(int[] arr, int target) {
+        HashSet<Integer> seen = new HashSet<>();
+
+        for (int num : arr) {
+            
+            int complement = target - num;
+            if (seen.contains(complement)) {
+                return new int[] {complement, num};  
+            }
+            
+            seen.add(num);
+        }
+
+        
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 7, 11, 15};
+        int target = 14;
+
+        int[] result = findPair(arr, target);
+
+        if (result != null) {
+            System.out.println("Пара элементов: " + result[0] + ", " + result[1]);
+        } else {
+            System.out.println("Пара с суммой " + target + " не найдена.");
+        }
+    }
+}
 
