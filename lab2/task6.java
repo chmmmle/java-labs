fe24bb5438e1486aab00a4957d636ba4bf79c56f
kamilya <120671774+chmmmle@users.noreply.github.com>
2025-02-06 19:45:54 +0200

    Update task6.java

--- lab2/task6.java
+++ lab2/task6.java
@@ -1,1 +1,25 @@
+public class Main {
+    public static int sumOfElements(int[][] array) {
+        int sum = 0; 
+        for (int i = 0; i < array.length; i++) { 
+            for (int j = 0; j < array[i].length; j++) { 
+                sum += array[i][j]; 
+            }
+        }
+        return sum; 
+    }
+
+    public static void main(String[] args) {
+        
+        int[][] array = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {-1, 4,9}
+        };
+
+        
+        int result = sumOfElements(array);
+        System.out.println("Сумма всех элементов в массиве: " + result);
+    }
+}
 
