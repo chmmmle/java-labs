64ee4a0534f1c0c26c18ee5256af54d728c46849
kamilya <120671774+chmmmle@users.noreply.github.com>
2025-02-06 19:47:46 +0200

    Update task7.java

--- lab2/task7.java
+++ lab2/task7.java
@@ -1,1 +1,29 @@
+public class Main {
+    public static int[] findMaxInRow(int[][] array) {
+        int[] result = new int[array.length]; 
+        for (int i = 0; i < array.length; i++) {
+            int max = array[i][0]; 
+            for (int j = 1; j < array[i].length; j++) {
+                if (array[i][j] > max) {
+                    max = array[i][j]; 
+                }
+            }
+            result[i] = max; 
+        }
+        return result;
+    }
 
+    public static void main(String[] args) {
+        int[][] array = {
+            {1, 3, 2, 4},
+            {10, 5, 8, 7}
+        };
+
+        int[] maxValues = findMaxInRow(array);
+
+        
+        for (int max : maxValues) {
+            System.out.print(max + " ");
+        }
+    }
+}
